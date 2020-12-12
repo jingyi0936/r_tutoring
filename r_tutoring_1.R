1d7044ce1d1accbce0341da85fd113d970abc6e4
Jingyi Cui <noorhelm419@outlook.com>
2020-12-11 20:59:00 -0800

    Chapter 3 part 1

---
+++ r_tutoring_1.R
@@ -0,0 +1,33 @@
+# Book name: R in a nutshell/R语言核心技术手册
+
+# help
+# ?
+
+1 + 2 + 3
+1 + 2 * 3
+(1 + 2) * 3 
+
+c(0, 1, 3, 5, 6, 8) # c means combine
+
+1:100 # show subscription
+
+# when lengths of 2 vectors is the same
+c(1, 2, 3, 1) + c(100, 200, 300, 400)
+c(1, 2, 3, 1) - c(100, 200, 300, 400)
+c(1, 2, 3, 1) * c(100, 200, 300, 400)
+c(1, 2, 3, 1) / c(100, 200, 300, 400)
+
+# when lengths of 2 vectors is different
+c(1, 2, 3, 1) + c(100, 200)
+1 / c(1, 2, 5, 100)
+c(9, 10, 45) + 1
+c(1, 2, 3) + c(100, 200)
+
+c("Hello", "World") # two char
+
+# how to add comments
+
+# how to save to a variable
+myFirstVar <- c("Hello", "World") # two char
+
+myFirstVar
